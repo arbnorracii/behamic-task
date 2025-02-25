f813ff2574de3f8892f7b23500b969112741237e
arbnorracii <arbnorii.r@gmail.com>
2025-02-25 23:25:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // Start Server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use("/api/auth", require("./routes/auth"));
